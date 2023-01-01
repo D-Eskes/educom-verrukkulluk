7535a2a1acbb5d61866d00b967c4c19ae5fb8429
D-Eskes <d.eskes.github@gmail.com>
2023-01-01 19:15:35 +0100

    database up to date

--- documents/database/db_verrukkulluk.sql
+++ documents/database/db_verrukkulluk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2022 at 02:30 PM
+-- Generation Time: Jan 01, 2023 at 07:14 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -34,21 +34,22 @@ CREATE TABLE `article` (
   `price` int(12) DEFAULT NULL,
   `unit` varchar(30) DEFAULT NULL,
   `quantity` int(12) DEFAULT NULL,
-  `calories` int(12) DEFAULT NULL
+  `calories` int(12) DEFAULT NULL,
+  `picture` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `name`, `description`, `price`, `unit`, `quantity`, `calories`) VALUES
-(1, 'tomaat', 'Een tomaat', 40, 'stuks', 1, 17700),
-(2, 'hamburger', 'Een stuk hamburger vlees ', 200, 'stuks', 1, 300000),
-(3, 'hamburger broodje', 'Een hamburger broodje', 125, 'stuks', 4, 600000),
-(4, 'mayonaise', 'Een pot mayonaise', 370, 'milliliter', 750, 5500000),
-(5, 'kipburger', 'Een stuk kip in de vorm van een burger', 250, 'stuks', 2, 450000),
-(6, 'visburger', 'Een stuk vis in de vorm van een burger', 300, 'stuks', 2, 400000),
-(7, 'veganburger', 'Een vegetarische burger', 100, 'stuks', 1, 150000);
+INSERT INTO `article` (`id`, `name`, `description`, `price`, `unit`, `quantity`, `calories`, `picture`) VALUES
+(1, 'tomaat', 'Een tomaat.', 40, 'stuks', 1, 17700, 'https://schoolfruit.nl/wp-content/uploads/2018/11/tomaat-los.jpg'),
+(2, 'hamburger', 'Een stuk hamburger vlees.', 200, 'stuks', 4, 600000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/L/534551.png'),
+(3, 'hamburger broodje', 'Een hamburger broodje.', 125, 'stuks', 6, 900000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/L/381721.png'),
+(4, 'mayonaise', 'Een pot mayonaise.', 370, 'milliliter', 650, 5500000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/ML/929595.png'),
+(5, 'kipburger', 'Een stuk kip in de vorm van een burger.', 250, 'stuks', 2, 450000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/ML/639652.png'),
+(6, 'visburger', 'Een stuk vis in de vorm van een burger.', 300, 'stuks', 4, 800000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/ML/640816.png'),
+(7, 'veganburger', 'Een vegetarische burger.', 100, 'stuks', 2, 300000, 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/L/911879.png');
 
 -- --------------------------------------------------------
 
@@ -69,9 +70,12 @@ CREATE TABLE `grocery` (
 --
 
 INSERT INTO `grocery` (`id`, `user_id`, `article_id`, `date_added`, `amount`) VALUES
-(1, 1, 1, '2022-12-22 10:37:20', 14),
-(2, 1, 5, '2022-12-22 10:37:20', 3),
-(6, 1, 3, '2022-12-22 13:22:15', 2);
+(23, 1, 7, '2022-12-23 08:55:04', 8),
+(24, 1, 3, '2022-12-23 08:55:04', 8),
+(25, 1, 4, '2022-12-23 08:55:04', 8),
+(26, 1, 6, '2022-12-30 14:46:00', 2),
+(27, 1, 5, '2022-12-30 14:46:22', 4),
+(28, 1, 2, '2022-12-30 14:46:30', 2);
 
 -- --------------------------------------------------------
 
@@ -143,10 +147,10 @@ CREATE TABLE `recipe` (
   `type_id` int(12) DEFAULT NULL,
   `user_id` int(12) DEFAULT NULL,
   `date_added` timestamp NOT NULL DEFAULT current_timestamp(),
-  `title` varchar(30) NOT NULL,
-  `description_short` varchar(300) NOT NULL,
-  `description_long` varchar(3000) NOT NULL,
-  `picture` varchar(300) NOT NULL
+  `title` varchar(30) DEFAULT NULL,
+  `description_short` varchar(300) DEFAULT NULL,
+  `description_long` varchar(3000) DEFAULT NULL,
+  `picture` mediumtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -154,10 +158,10 @@ CREATE TABLE `recipe` (
 --
 
 INSERT INTO `recipe` (`id`, `kitchen_id`, `type_id`, `user_id`, `date_added`, `title`, `description_short`, `description_long`, `picture`) VALUES
-(1, 9, 6, 1, '2022-12-20 23:00:00', 'hamburger', 'Een lekkere hamburger.', 'Een heel lekkere hamburger.', ''),
-(2, 4, 7, 1, '2022-12-21 09:31:12', 'visburger', 'Een lekkere visburger.', 'Een heel lekkere visburger.', ''),
-(3, 4, 6, 1, '2022-12-21 09:31:12', 'kipburger', 'Een lekkere kipburger.', 'Een heel lekkere kipburger.', ''),
-(4, 5, 8, 1, '2022-12-21 09:31:12', 'veganburger', 'Een lekkere veganburger.', 'Een heel lekkere veganburger.', '');
+(1, 9, 6, 1, '2022-12-20 23:00:00', 'hamburger', 'Een lekkere hamburger.', 'Een heel lekkere hamburger.', 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/LW/r_pm0080.jpg'),
+(2, 4, 7, 1, '2022-12-21 09:31:12', 'visburger', 'Een lekkere visburger.', 'Een heel lekkere visburger.', 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/LW/r_pm0271.jpg'),
+(3, 4, 6, 1, '2022-12-21 09:31:12', 'kipburger', 'Een lekkere kipburger.', 'Een heel lekkere kipburger.', 'https://img.static-rmg.be/a/food/image/q75/w1280/h720/1084076/tandoori-kipburger.jpg'),
+(4, 5, 8, 1, '2022-12-21 09:31:12', 'veganburger', 'Een lekkere veganburger.', 'Een heel lekkere veganburger.', 'https://www.plus.nl/INTERSHOP/static/WFS/PLUS-Site/-/PLUS/nl_NL/product/LW/r_kma00339.jpg');
 
 -- --------------------------------------------------------
 
@@ -180,28 +184,36 @@ CREATE TABLE `recipe_info` (
 --
 
 INSERT INTO `recipe_info` (`id`, `record_type`, `recipe_id`, `user_id`, `date`, `field_number`, `field_text`) VALUES
-(1, 'P', 1, NULL, '2022-12-20 23:00:00', 1, 'Bak de hamburgers'),
-(2, 'P', 1, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de hamburgers op de broodjes'),
-(3, 'P', 1, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees'),
+(1, 'P', 1, NULL, '2022-12-20 23:00:00', 1, 'Bak de hamburgers.'),
+(2, 'P', 1, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de hamburgers op de broodjes.'),
+(3, 'P', 1, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees.'),
 (4, 'C', 1, 1, '2022-12-21 09:12:32', NULL, 'Lekkerste hamburger ooit gegeten!'),
 (5, 'R', 1, NULL, '2022-12-21 09:16:26', 4, NULL),
 (6, 'R', 1, NULL, '2022-12-21 09:16:26', 5, NULL),
-(8, 'P', 3, NULL, '2022-12-20 23:00:00', 1, 'Bak de kipburgers'),
-(9, 'P', 3, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de kipburgers op de broodjes'),
-(10, 'P', 3, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees'),
+(8, 'P', 3, NULL, '2022-12-20 23:00:00', 1, 'Bak de kipburgers.'),
+(9, 'P', 3, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de kipburgers op de broodjes.'),
+(10, 'P', 3, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees.'),
 (11, 'C', 3, 1, '2022-12-21 09:12:32', NULL, 'Lekkerste kipburger ooit gegeten!'),
-(12, 'P', 2, NULL, '2022-12-20 23:00:00', 1, 'Bak de visburgers'),
-(13, 'P', 2, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de visburgers op de broodjes'),
-(14, 'P', 2, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees'),
+(12, 'P', 2, NULL, '2022-12-20 23:00:00', 1, 'Bak de visburgers.'),
+(13, 'P', 2, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de visburgers op de broodjes.'),
+(14, 'P', 2, NULL, '2022-12-20 23:00:00', 3, 'Doe een schepje mayonaise op elk broodje en klaar is Kees.'),
 (15, 'C', 2, 1, '2022-12-21 09:12:32', NULL, 'Lekkerste visburger ooit gegeten!'),
-(16, 'P', 4, NULL, '2022-12-20 23:00:00', 1, 'Bak de veganburgers'),
-(17, 'P', 4, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de veganburgers op de broodjes'),
-(18, 'P', 4, NULL, '2022-12-20 23:00:00', 3, 'Doe een flinke schepje mayonaise op elk broodje en klaar is Kees'),
+(16, 'P', 4, NULL, '2022-12-20 23:00:00', 1, 'Bak de veganburgers.'),
+(17, 'P', 4, NULL, '2022-12-20 23:00:00', 2, 'Snij de broodjes en plaats de veganburgers op de broodjes.'),
+(18, 'P', 4, NULL, '2022-12-20 23:00:00', 3, 'Doe een flinke schepje mayonaise op elk broodje en klaar is Kees.'),
 (19, 'C', 4, 1, '2022-12-21 09:12:32', NULL, 'Die flinke klodder mayonaise maakte het nog enigszins te eten...'),
 (20, 'R', 4, NULL, '2022-12-21 09:16:26', 2, NULL),
 (21, 'R', 4, NULL, '2022-12-21 09:16:26', 3, NULL),
-(22, 'F', 2, 1, '2022-12-21 12:43:55', NULL, NULL),
-(36, 'F', 1, 1, '2022-12-21 16:51:05', NULL, NULL);
+(247, 'R', 1, NULL, '2022-12-30 10:24:44', 4, NULL),
+(388, 'R', 3, NULL, '2022-12-30 11:13:36', 5, NULL),
+(390, 'R', 2, NULL, '2022-12-30 11:24:42', 4, NULL),
+(415, 'R', 2, NULL, '2022-12-30 11:25:24', 3, NULL),
+(423, 'F', 1, 1, '2022-12-30 13:03:55', NULL, NULL),
+(426, 'R', 2, NULL, '2023-01-01 18:13:04', 1, NULL),
+(427, 'R', 2, NULL, '2023-01-01 18:13:06', 5, NULL),
+(428, 'R', 2, NULL, '2023-01-01 18:13:07', 5, NULL),
+(429, 'R', 2, NULL, '2023-01-01 18:13:08', 5, NULL),
+(430, 'R', 2, NULL, '2023-01-01 18:13:08', 5, NULL);
 
 -- --------------------------------------------------------
 
@@ -222,7 +234,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `picture`) VALUES
-(1, 'DaanEskes', '1234', 'daaneskes57@gmail.com', NULL);
+(1, 'Justin123', '1234', 'justin@trudeau.com', 'https://media.licdn.com/dms/image/C4E03AQGD-PJnXZmU8g/profile-displayphoto-shrink_800_800/0/1629053584973?e=2147483647&v=beta&t=FNFUVJ2bR7fV3a7zsgSD74d8VsV-Tg2hGzxrlTtEE0s');
 
 --
 -- Indexes for dumped tables
@@ -292,7 +304,7 @@ ALTER TABLE `article`
 -- AUTO_INCREMENT for table `grocery`
 --
 ALTER TABLE `grocery`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `ingredient`
@@ -316,7 +328,7 @@ ALTER TABLE `recipe`
 -- AUTO_INCREMENT for table `recipe_info`
 --
 ALTER TABLE `recipe_info`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=431;
 
 --
 -- AUTO_INCREMENT for table `user`
